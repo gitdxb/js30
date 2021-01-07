d092c01f5583054a2425e9ce220a9e4c361bb5b5
gitdxb <trantambls@gmail.com>
2021-01-07 23:38:10 +0400

    update

--- js-sandbox/script.js
+++ js-sandbox/script.js
@@ -1,2 +1,13 @@
-var date = new Date();
-document.body.innerHTML = "<h1>Today is " + date + "<h1>"; 
+/* JS  data types:
+1. Numeric
+2. String
+3. null
+4. Boolean: true/false
+5. undefined => a var without set to anything */
+
+var negInteger = -3.14159265;
+var escQuote = "Quotes can also be \"escaped\"."
+var theSunIsWarm = true;
+var emptyInside = null;
+var justAnotherVariable;
+
