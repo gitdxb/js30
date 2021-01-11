62ec9672b39654d883c9dca2ef668437377451be
gitdxb <trantambls@gmail.com>
2021-01-11 23:46:53 +0400

    update new lesson

--- js-sandbox/script.js
+++ js-sandbox/script.js
@@ -1,3 +1,44 @@
+function findBiggestFraction(a,b) {
+    a > b ? console.log("a: ", a) : console.log("b: ", b);
+}
+
+var a = 3 / 4;
+var b = 5 / 7;
+
+findBiggestFraction();
+
+
+
+// Regular function, called explicitly by name
+// function multiply() {
+//     var result = 3 * 4;
+//     console.log("3 multiply by 4 is ", result);
+// }
+// multiply();
+
+// Anonymous function stored in variable
+// Invoked by calling the variable as a function
+// var divided = function () {
+//     var result = 3 / 4;
+//     console.log("3 divide by 4 is ", result);
+// }
+// divided();
+
+// Immdediately invoked func expression
+// Run as soon as the browser finds it:
+// (function () {
+//     var result = 12 / 0.75;
+//     console.log("12 divided by 0.75 is ", result);
+// }())
+
+// function nhanHaiSo(a, b) {
+//     var ketQua = a * b;
+//     console.log(ketQua);
+//     return ketQua;
+// }
+
+// var multiplied = nhanHaiSo(3, 4);
+
 /* JS  data types:
 1. Numeric
 2. String
@@ -15,29 +56,29 @@
 // console.log(humanSum);
 
 // Conditional statement
-var a = 5;
-var b = "5";
-var theNumbersMatch;
+// var a = 5;
+// var b = "5";
+// var theNumbersMatch;
 
-if (a === b) {
-    theNumbersMatch = true;
-} else {
-    theNumbersMatch = false;
-}
+// if (a === b) {
+//     theNumbersMatch = true;
+// } else {
+//     theNumbersMatch = false;
+// }
 
-console.log("The numbers match: " + theNumbersMatch);
+// console.log("The numbers match: " + theNumbersMatch);
 
 // Ternary Operator
 // condition ? true: false
-a == b ? theNumbersMatch = true : theNumbersMatch = false;
+// a == b ? theNumbersMatch = true : theNumbersMatch = false;
 
-var pens;
-pens = ["red", "blue", "green", "orange"];
-console.log("Before: ", pens);
+// var pens;
+// pens = ["red", "blue", "green", "orange"];
+// console.log("Before: ", pens);
 
 // Properties
 // Get a property of an object by name
-console.log("Array length: ", pens.length);
+// console.log("Array length: ", pens.length);
 
 // METHODS
 // Reverse the array
@@ -62,14 +103,14 @@ console.log("Array length: ", pens.length);
 //console.log("After: ", pens);
 
 // Create a copy of an array. Assign to a new var
-var newPens = pens.slice();
-console.log("New pens: ", newPens);
+// var newPens = pens.slice();
+// console.log("New pens: ", newPens);
 
 
 // Return the first element that match the search 
-var result = pens.indexOf("orange", 0);
-console.log("The value is: ", pens[result]);
+// var result = pens.indexOf("orange", 0);
+// console.log("The value is: ", pens[result]);
 
 // Return the items in an array as a comma separated:
-var arrayString = pens.join(", ");
-console.log("String from array: ", arrayString);
+// var arrayString = pens.join(", ");
+// console.log("String from array: ", arrayString);
