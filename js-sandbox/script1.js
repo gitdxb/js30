4beea88458697ab021cebbdbe4ea985721c88da0
gitdxb <trantambls@gmail.com>
2021-01-23 13:14:09 +0400

    new js

--- js-sandbox/script1.js
+++ js-sandbox/script1.js
@@ -24,3 +24,19 @@ FEATURE.appendChild(captionElement);
 
 THEIMAGE.setAttribute("alt", "");
 
+// toggle the button
+const CTABUTTON = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
+
+CTABUTTON.classList.remove("hide");
+ALERT.classList.add("hide");
+
+function reveal(e) {
+    // after click the page stay the same without scrolling up
+    e.preventDefault();
+
+    CTABUTTON.classList.toggle("hide");
+    ALERT.classList.toggle("hide");
+}
+
+CTABUTTON.onclick = reveal;
